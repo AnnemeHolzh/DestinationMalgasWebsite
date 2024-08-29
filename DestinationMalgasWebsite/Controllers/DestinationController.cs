a7d8e4c03bfefd0c1b12fa58fd7bd0bb23fdd07b
Anneme <102529703+AnnemeHolzh@users.noreply.github.com>
2024-08-29 22:09:10 +0200

    halfway done with the destinatoin sub pages!

--- DestinationMalgasWebsite/Controllers/DestinationController.cs
+++ DestinationMalgasWebsite/Controllers/DestinationController.cs
@@ -4,14 +4,63 @@ namespace DestinationMalgasWebsite.Controllers
 {
     public class DestinationController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<DestinationController> _logger;
 
-        public DestinationController(ILogger<HomeController> logger)
+        public DestinationController(ILogger<DestinationController> logger)
         {
             _logger = logger;
         }
 
         public IActionResult Destination()
+        {
+            return View();
+        }
+
+        public IActionResult Malgas()
+        {
+            return View();
+        }
+
+        public IActionResult MalgasFerry()
+        {
+            return View();
+        }
+
+        public IActionResult BoathousePizzaAndPub()
+        {
+            return View();
+        }
+
+        public IActionResult BushPub()
+        {
+            return View();
+        }
+        public IActionResult SijnnWineEstate()
+        {
+            return View();
+        }
+
+        public IActionResult Grunters()
+        {
+            return View();
+        }
+
+        public IActionResult DeHoop()
+        {
+            return View();
+        }
+
+        public IActionResult TheFigTree()
+        {
+            return View();
+        }
+
+        public IActionResult Bites()
+        {
+            return View();
+        }
+
+        public IActionResult TheTradingPost()
         {
             return View();
         }
--- DestinationMalgasWebsite/wwwroot/js/site.js
+++ DestinationMalgasWebsite/wwwroot/js/site.js
@@ -19,3 +19,20 @@ window.onload = adjustScale;
 
 // Adjust the scale when the window is resized
 window.onresize = adjustScale;
+
+let lastScrollTop = 0;
+const navbar = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scrolling down
+        navbar.classList.add('header-hidden');
+    } else {
+        // Scrolling up
+        navbar.classList.remove('header-hidden');
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+});
