e47793172b3831eb507ca60e25d42d8101146cf3
Anneme <102529703+AnnemeHolzh@users.noreply.github.com>
2024-09-01 19:01:50 +0200

    landing fully responsive

--- DestinationMalgasWebsite/wwwroot/js/LandingSlideshowLogic.js
+++ DestinationMalgasWebsite/wwwroot/js/LandingSlideshowLogic.js
@@ -2,44 +2,44 @@
     // Section 1 Slideshow
     const slides = [
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide1.png",
-            backgroundImage: "/Assets/Landing/Img/DM_Offer_Bg.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide1.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_Offer_Bg.jpg",
             description: "Get to know us! We aim to provide a comprehensive service to ensure your stay is memorable.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide01.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide2.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Accomidation.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide2.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Accomidation.jpg",
             description: "Choose from our variety of houses, cottages, and houseboats.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide02.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide3.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Boating1.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide3.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Boating1.jpg",
             description: "Take advantage of our private jetties and explore the Breede River, offering boat charters and boat rentals.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide03.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide4.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_ServiceProvider.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide4.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_ServiceProvider.jpg",
             description: "Supporting our local service providers, contact us for assistance or let us do the marketing for you.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide04.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide5.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Bait.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide5.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Bait.jpg",
             description: "Stock up on everything you need for a successful fishing trip.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide05.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide6.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Dining.jpeg",
+            svgImage: "/Assets/Landing/TopText/DM_Slide6.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Dining.jpg",
             description: "Visit The Boathouse Pub and Pizza for a delightful meal or choose from a variety of other options.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide06.png"
         },
         {
-            svgImage: "/Assets/Landing/TopText/DM_Slide7.png",
-            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Hub.png",
+            svgImage: "/Assets/Landing/TopText/DM_Slide7.svg",
+            backgroundImage: "/Assets/Landing/Img/DM_TopSection_Hub.jpg",
             description: "Our hub provides all the information you need about local attractions and activities, office and shop.",
             slideNumber: "/Assets/Landing/TopText/Numbers/slide07.png"
         }
@@ -114,58 +114,3 @@
     window.addEventListener('resize', updateCarousel);
 });
 
-// Section 3 Slideshow
-document.addEventListener('DOMContentLoaded', () => {
-    const reasons = [
-        {
-            h2: "1. Stunning Scenery",
-            body: "Explore numerous hiking trails that cater to all levels of fitness. <br><br>From lush riverbanks to rolling hills, every view is a postcard moment.",
-            image: "/Assets/Landing/Img/VisitCarousel/reasons1.png"
-        },
-        {
-            h2: "2. Hiking Trails",
-            body: "Discover the historical landmarks and learn about the cultural heritage of Malgas. <br><br>Discover the diverse flora and fauna, and perhaps spot some local wildlife.",
-            image: "/Assets/Landing/Img/VisitCarousel/reasons2.png"
-        },
-        {
-            h2: "3. Whale Watching",
-            body: "Visit during whale season to witness the magnificent spectacle of these gentle giants. <br><br>Infanta and Witsand are renowned for their whale watching opportunities.",
-            image: "/Assets/Landing/Img/VisitCarousel/reason3.png"
-        },
-        {
-            h2: "4. Fishing",
-            body: "The Breede River is teeming with fish like Cob and Garrick. <br><br>Whether you're a seasoned angler or a beginner, you'll find great spots for fishing.",
-            image: "/Assets/Landing/Img/VisitCarousel/reason4.png"
-        },
-        {
-            h2: "5. Bird Watching",
-            body: "With a rich variety of bird species, including fish eagles and kingfishers, Malgas is a haven for bird watchers.",
-            image: "/Assets/Landing/Img/VisitCarousel/reason5.png"
-        },
-        {
-            h2: "6. Local Attractions",
-            body: "Explore historical monuments, visit local pubs and restaurants, and tour nearby wineries.<br><br>There's always something new to discover.",
-            image: "/Assets/Landing/Img/VisitCarousel/reason6.png"
-        }
-    ];
-
-    let currentReason = 0;
-    const reasonH2Element = document.getElementById('reason-h2');
-    const reasonBodyElement = document.getElementById('reason-body');
-    const reasonImageElement = document.getElementById('reason-image');
-
-    function changeReasonSlide(direction) {
-        if (direction === 'next') {
-            currentReason = (currentReason + 1) % reasons.length;
-        } else if (direction === 'prev') {
-            currentReason = (currentReason - 1 + reasons.length) % reasons.length;
-        }
-
-        reasonH2Element.innerHTML = reasons[currentReason].h2;
-        reasonBodyElement.innerHTML = reasons[currentReason].body;
-        reasonImageElement.src = reasons[currentReason].image;
-    }
-
-    document.querySelector('.section3 .left-button').addEventListener('click', () => changeReasonSlide('prev'));
-    document.querySelector('.section3 .right-button').addEventListener('click', () => changeReasonSlide('next'));
-});
